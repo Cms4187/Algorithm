1584529e59d2149fa606ca2f0f73d0285b166240
Cms4187 <72210821+Cms4187@users.noreply.github.com>
2022-02-09 20:09:02 +0900

    지수 연산
    f-string 사용하여 출력 또는 큰 자릿수가 과학적 표기법으로 출력되는 것 값 바꾸기

--- BackJoon/Bronze/Number_Cal.py
+++ BackJoon/Bronze/Number_Cal.py
@@ -8,4 +8,10 @@ print(f'{2**(-n):.250f}')
 #지정하지않고 .값 으로 하면 해당 값 출력이 끝나는 지점까지만 출력
 print(f'{2**(-n):.{n}f}')
 
-
+result= "%.250f"% (2**(-n))
+last=len(result) #result의 길이 저장
+for i in range(last-1, 1, -1): #마지막부터 앞까지 오름차순으로 출력
+    if result[i]!='0':
+        last=i
+        break
+print(result[:last+1]) #맨 앞부터 마지막까지만 출력되게 슬라이싱
