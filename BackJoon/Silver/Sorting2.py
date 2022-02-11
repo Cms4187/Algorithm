2a1466a0f1fc7411146f72a555074fe1978d0bfd
Cms4187 <72210821+Cms4187@users.noreply.github.com>
2022-02-11 21:30:43 +0900

    오름차순 정렬
    sys사용하기

--- BackJoon/Silver/Sorting2.py
+++ BackJoon/Silver/Sorting2.py
@@ -1,8 +1,12 @@
+#문제에 입력값이 1~100만이므로 그냥 입력받으면 시간초과
+#sys를 사용하기
+import sys
 N = int(input())
 NL = []
 
 for i in range(N):
-    NL.append(int(input()))
+    NL.append(int(sys.stdin.readline()))
+    #NL.append(int(input()))
 
 NL.sort()
 
