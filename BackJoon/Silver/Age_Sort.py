a48cd4d399d01b7f59200a25da35b995f7076899
Cms4187 <72210821+Cms4187@users.noreply.github.com>
2022-02-14 17:05:19 +0900

    Update Age_Sort.py

--- BackJoon/Silver/Age_Sort.py
+++ BackJoon/Silver/Age_Sort.py
@@ -1,9 +1,21 @@
-N = int(input())
+import sys
+input = sys.stdin.readline()
+
+N = int(input().rstrip())
 Info = []
-
-for _ in range(N):
+print(N)
+for i in range(N):
     Age, Name = map(str, input().split())
-    Info.append(Age, Name)
+    Info.append((Age, Name))
+    #앞의 값은 나이값 = int값 이므로, 값을 넣어준 다음에 int값으로 변경해줌
+    Info[i][0] = int(Info[i][0])
 
-print(Info)
+Info.sort(key=lambda x:x[0])
+for i in Info:
+    print(i[0],i[1])
+
+
+
+
+
 
