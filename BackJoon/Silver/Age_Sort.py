d21b1c1ce92fecbf8a3004b051dc2b447ca92186
Cms4187 <72210821+Cms4187@users.noreply.github.com>
2022-02-14 17:15:09 +0900

    나이 순 정렬
    10814번

--- BackJoon/Silver/Age_Sort.py
+++ BackJoon/Silver/Age_Sort.py
@@ -1,12 +1,12 @@
+#sys로 입력받아 빠르게 입력받음
 import sys
 input = sys.stdin.readline
 
 N = int(input().rstrip())
 Info = []
-print(N)
 for i in range(N):
-    Age, Name = map(str, input().split())
-    Info.append((Age, Name))
+    #리스트에 띄어쓰기를 기준으로 나눠 입력받음
+    Info.append(list(input().split()))
     #앞의 값은 나이값 = int값 이므로, 값을 넣어준 다음에 int값으로 변경해줌
     Info[i][0] = int(Info[i][0])
 
