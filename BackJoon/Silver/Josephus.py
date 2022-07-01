3a3b9135c11fa96549f203102b391911426be845
Cms4187 <72210821+Cms4187@users.noreply.github.com>
2022-07-01 18:51:20 +0900

    요세푸스
    1158번

--- BackJoon/Silver/Josephus.py
+++ BackJoon/Silver/Josephus.py
@@ -6,22 +6,27 @@ Peo = [_ for _ in range(1, N+1)]
 pop_Peo = []
 del_k = 0
 
+
+
 while Peo:
     #K-1 번째 인덱스를 제거할 사람으로 지정
     del_k += K-1
     if del_k >= len(Peo):
         del_k = del_k % len(Peo)
 
-    pop_Peo.append(Peo[del_k])
+    #pop_Peo.append(Peo[del_k])
+    pop_Peo.append(str(Peo[del_k]))
     Peo.pop(del_k)
 
-print("<", end="")
+# print("<", end="")
 
-for i in range(len(pop_Peo)):
-    if i == len(pop_Peo)-1:
-        print(pop_Peo[i], end="")
-        break
-    print(pop_Peo[i], end=", ")
+# for i in range(len(pop_Peo)):
+#     if i == len(pop_Peo)-1:
+#         print(pop_Peo[i], end="")
+#         break
+#     print(pop_Peo[i], end=", ")
 
-print(">", end="")
+# print(">", end="")
 
+#17줄 append할 때 str형으로 append하고, join함수써서 출력시 1줄로 출력 가능
+print("<",', '.join(pop_Peo),">", sep="")
